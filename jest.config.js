9edc1d714233a6ae52f1a3b2c6998367b2ceed36
Emad Rashdan <emadiga@gmail.com>
2021-07-09 23:48:55 +0300

    Fix jest configuration

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    moduleNameMapper: {
+        '^/opt/utils': "<rootDir>/src/layers/functions"
+    },
+    modulePaths: [
+        "<rootDir>/src/layers/dependencies/nodejs/node_modules/"
+    ]
+}
